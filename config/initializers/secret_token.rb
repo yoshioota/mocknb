5ddd8fd00191a2c0add23cf5403d08770d97f826
Yoshi Oota <yoshi.oota@gmail.com>
2012-02-15 22:10:31 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mocknb::Application.config.secret_token = 'd1aeccd500003ce4924e03703048d9cf5a98c0c69cf7898775ef8011a6d76ad4d728625ff0e624103a2b80eb146cb96b690578c70b97217e6209cb913237e377'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Mocknb::Application.config.session_store :cookie_store, key: '_mocknb_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Mocknb::Application.config.session_store :active_record_store
